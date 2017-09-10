c1ad97c4ee48bd59d5682688327d694640d09613
gkheptonstall <gk.heptonstall@gmail.com>
2017-09-10 17:51:54 +0800

    add main file

---
+++ src/main/java/org/heliogator/maui/MauiMain.java
@@ -0,0 +1,12 @@
+package org.heliogator.maui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MauiMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MauiMain.class);
+    }
+}
