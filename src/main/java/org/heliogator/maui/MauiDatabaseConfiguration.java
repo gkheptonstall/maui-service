f636ce2b581c5e58a6e4b3be4405d6d440b29c5b
gkheptonstall <gk.heptonstall@gmail.com>
2017-09-10 18:17:22 +0800

    cleanup pom and add config files

---
+++ src/main/java/org/heliogator/maui/MauiDatabaseConfiguration.java
@@ -0,0 +1,7 @@
+package org.heliogator.maui;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MauiDatabaseConfiguration {
+}
--- src/main/java/org/heliogator/maui/MauiMain.java
+++ src/main/java/org/heliogator/maui/MauiMain.java
@@ -1,9 +1,11 @@
 package org.heliogator.maui;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ImportAutoConfiguration(value = {MauiDatabaseConfiguration.class})
 public class MauiMain {
 
     public static void main(String[] args) {
