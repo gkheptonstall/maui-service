5adefd5663879f987dc2b0612753d20e495b2391
gkheptonstall <gk.heptonstall@gmail.com>
2017-09-26 00:19:36 +0800

    add lambda expressions

--- src/test/java/org/heliogator/maui/service/PieSequenceServiceTest.java
+++ src/test/java/org/heliogator/maui/service/PieSequenceServiceTest.java
@@ -1,18 +1,17 @@
 package org.heliogator.maui.service;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.heliogator.maui.MauiMainTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = MauiMainTest.class)
